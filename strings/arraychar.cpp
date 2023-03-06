d50b551f61dcd66d13d17a4a1692ace42677cd4f
shailendra parihar <113036851+shelu18@users.noreply.github.com>
2023-03-06 08:35:19 +0530

    Add files via upload

---
+++ strings/arraychar.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   // cout<<"Hello World";
+   char ch[10];
+   cout<<"enter your name"<<endl;
+   cin>>ch;
+   
+cout<<"you have entered "<<ch;
+    return 0;
+}
